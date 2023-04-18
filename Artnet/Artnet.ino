f7c9b7d2f9ec96c702fa768d4c9264f37407040e
Cori V <21335163+coriolan-v@users.noreply.github.com>
2023-04-18 23:00:42 +0100

    added working artnet code

--- Artnet/Artnet.ino
+++ Artnet/Artnet.ino
@@ -6,7 +6,6 @@
 // http://forum.pjrc.com/threads/24688-Artnet-to-OctoWS2811?p=55589&viewfull=1#post55589
 
 #include <Artnet.h>
-// DON'T FORGET TO CHANGE ANY REFERENCES OF Ethernet.h and EthernetUdp.h in Arnet.h to NativeEthernet.h and NativeEthernetUdp.h
 #include <NativeEthernet.h>
 #include <NativeEthernetUdp.h>
 #include <SPI.h>
@@ -15,19 +14,21 @@
 // Ideas for improving performance with WIZ820io / WIZ850io Ethernet:
 // https://forum.pjrc.com/threads/45760-E1-31-sACN-Ethernet-DMX-Performance-help-6-Universe-Limit-improvements
 
+const int numPins = 1;
+byte pinList[numPins] = {23};
+
 // OctoWS2811 settings
-const int ledsPerStrip = 300; // change for your setup
-const byte numStrips= 5; // change for your setup
+const int ledsPerStrip = 492; // change for your setup
+const byte numStrips= 1; // change for your setup
 DMAMEM int displayMemory[ledsPerStrip*6];
 int drawingMemory[ledsPerStrip*6];
 const int config = WS2811_GRB | WS2811_800kHz;
-//const int numPins = 1;
-byte pinList[numStrips] = {23, 24, 25, 26, 27};
-OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config,  numStrips, pinList);
+//OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
+OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config, numPins, pinList);
 
 // Artnet settings
 Artnet artnet;
-const int startUniverse = 1; // CHANGE FOR YOUR SETUP most software this is 1, some software send out artnet first universe as zero.
+const int startUniverse = 0; // CHANGE FOR YOUR SETUP most software this is 1, some software send out artnet first universe as zero.
 const int numberOfChannels = ledsPerStrip * numStrips * 3; // Total number of channels you want to receive (1 led = 3 channels)
 byte channelBuffer[numberOfChannels]; // Combined universes into a single array
 
@@ -38,7 +39,7 @@ bool sendFrame = 1;
 
 // Change ip and mac address for your setup
 byte ip[] = {192, 168, 1, 3};
-byte mac[] = {0x04, 0xE9, 0xE5, 0x00, 0x69, 0xEC};
+byte mac[] = {0x04, 0xE9, 0xE5, 0x00, 0x69, 0xED};
 
 void setup()
 {
@@ -65,15 +66,15 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* d
   if (universe < maxUniverses)
     universesReceived[universe] = 1;
 
-  // for (int i = 0 ; i < maxUniverses ; i++)
-  // {
-  //   if (universesReceived[i] == 0)
-  //   {
-  //     //Serial.println("Broke");
-  //     sendFrame = 0;
-  //     break;
-  //   }
-  // }
+  for (int i = 0 ; i < maxUniverses ; i++)
+  {
+    if (universesReceived[i] == 0)
+    {
+      //Serial.println("Broke");
+      sendFrame = 0;
+      break;
+    }
+  }
 
   // read universe and put into the right part of the display buffer
   for (int i = 0 ; i < length ; i++)
